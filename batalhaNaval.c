2e6503a9da7748ed6156487a698371d91505ccff
José Luiz Neto <133919788+netoMendes21@users.noreply.github.com>
2025-03-14 22:28:51 +0000

    Nível Novato feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,12 +4,77 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+void tabuleiro()
+{
+    int navio[1] = {3};
+    int tabuleiroNaval[10][10] = {0};
+
+    for (int i = 0; i < 10; i++)
+    {
+        tabuleiroNaval[0][0] = navio[0];
+        tabuleiroNaval[0][1] = navio[0];
+        tabuleiroNaval[0][2] = navio[0];
+
+        tabuleiroNaval[2][0] = navio[0];
+        tabuleiroNaval[3][0] = navio[0];
+        tabuleiroNaval[4][0] = navio[0];
+
+        printf("%2d ", i + 1);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiroNaval[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+void coluna()
+{
+    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    printf("\n");
+    printf("  ");
+    for (int i = 0; i < 10; i++)
+    {
+        printf(" %c", colunas[i]);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int opcao;
+    do
+    {
+        printf("\n----------------Bem Vindo ao Batalha Naval----------------\n");
+        printf("1. Regras do Jogo\n");
+        printf("2. Jogar\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
+        switch (opcao)
+        {
+        case 1:
+            printf("Em manuntenção....\n");
+            break;
+
+        case 2:
+            coluna();
+            tabuleiro();
+            break;
+
+        default:
+            printf("\n----------------###OPÇÃO INVÁLIDA###----------------\n\n");
+            break;
+        }
+    } while (opcao != 3 || opcao > 3);
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
@@ -25,7 +90,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
